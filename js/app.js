e4da93c0cc80bedab2e336817ca27bc910b92e8f
francoromeo <francoromeo5012@gmail.com>
2024-01-30 00:42:51 -0300

    menu mobile

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ function copyMenu(){
   var navPlace = document.querySelector('.off-canvas nav');
   navPlace.innerHTML = mainNav.innerHTML;
 
-  var topNav = document.querySelector('.header-top');
+  var topNav = document.querySelector('.header-top .wrapper');
   var topPlace = document.querySelector('.off-canvas .thetop-nav')
   topPlace.innerHTML = topNav.innerHTML;
 
