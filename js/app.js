084e9678a5ee3f2096acd619b2f4921cb7ddbab7
francoromeo <francoromeo5012@gmail.com>
2024-01-29 23:55:14 -0300

    js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function copyMenu(){
+  var dpCategory = document.querySelector('.dpt-cat');
+  var dptPlace = document.querySelector('.departments');
+  dptPlace.innerHTML = dpCategory.innerHTML;
+
+  var mainNav = document.querySelector('.header-nav nav');
+  var navPlace = document.querySelector('.off-canvas nav');
+  navPlace.innerHTML = mainNav.innerHTML;
+
+  var topNav = document.querySelector('.header-top');
+  var topPlace = document.querySelector('.off-canvas .thetop-nav')
+  topPlace.innerHTML = topNav.innerHTML;
+
+}
+copyMenu();
