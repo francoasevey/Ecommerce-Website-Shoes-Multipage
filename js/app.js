effa370a2f0aef22c766bcda203a85b22381bb90
francoromeo <francoromeo5012@gmail.com>
2024-01-30 23:54:39 -0300

    swiper

--- js/app.js
+++ js/app.js
@@ -33,3 +33,10 @@ function toggle(e){
   if (this.closest('.has-child').classList != 'expand');
   this.closest('.has-child').classList.toggle('expand')
 }
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper.pagination'
+  },
+});
