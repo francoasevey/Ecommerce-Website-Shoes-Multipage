d99a164e5eaf51514af750985ded801127e8f407
francoromeo <francoromeo5012@gmail.com>
2024-02-08 13:15:31 -0300

    menushow

--- js/app.js
+++ js/app.js
@@ -90,3 +90,22 @@ for (let x = 0; x < stocks.length; x++){
 
   stocks[x].querySelector('.available').style.width = percent + "%"
 }
+
+const FtoShow = '.filter';
+const Fpopup = document.querySelector(FtoShow);
+const Ftrigger = document.querySelector('.filter-trigger');
+
+Ftrigger.addEventListener('click', () =>{
+  setTimeout(() =>{
+    if (!Fpopup.classList.contains('show')) {
+      Fpopup.classList.add('show')
+    }
+  }, 250)
+})
+
+document.addEventListener('click', (e) =>{
+  const  isClosest = e.target.closest(FtoShow);
+  if (!isClosest && Fpopup.classList.contains('show')) {
+    Fpopup.classList.remove('show')
+  }
+})
