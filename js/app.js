46a3d370b56d56d01abd02fc76fd4213b1116a31
francoromeo <francoromeo5012@gmail.com>
2024-02-11 23:42:31 -0300

    arrow up

--- js/app.js
+++ js/app.js
@@ -29,35 +29,7 @@ var countDownDate = new Date("Dec 13, 2024 15:13:00").getTime();
       }, 1000);
 
 
-      var countDownDate2 = new Date("Dec 13, 2024 15:13:00").getTime();
-      
-      // Update the count down every 1 second
-      var x = setInterval(function() {
-      
-        // Get today's date and time
-        var now = new Date().getTime();
-      
-        // Find the distance between now and the count down date
-        var distance = countDownDate2 - now;
-      
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      
-        // Display the result in the element with id="demo"
-        document.getElementById("days").innerHTML = days + "d";
-        document.getElementById("hours").innerHTML = hours + "h";
-        document.getElementById("minutes").innerHTML = minutes + "m";
-        document.getElementById("seconds").innerHTML = seconds + "s";
-      
-        // If the count down is finished, write some text
-        if (distance < 0) {
-          clearInterval(x);
-          document.getElementById("demonio").innerHTML = "EXPIRED";
-        }
-      }, 1000);
+    
       
       
 function copyMenu(){
